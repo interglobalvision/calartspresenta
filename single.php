c862c572c5ce473846989b60568132904c230a39
mikeyrayvon <mikey@interglobal.vision>
2018-04-09 12:02:56 -0500

    add data to single

--- single.php
+++ single.php
@@ -3,22 +3,41 @@ get_header();
 ?>
 
 <main id="main-content">
-  <section id="content">
+  <section id="posts">
     <div class="container">
-      <div class="grid-row">
-
+      <div id="content">
 <?php
 if (have_posts()) {
   while (have_posts()) {
     the_post();
+
+    $duration = get_post_meta($post->ID, '_igv_film_duration', true);
+    $year = get_post_meta($post->ID, '_igv_film_year', true);
+    $directors = wp_get_post_terms($post->ID, 'director');
 ?>
 
-        <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
-
-          <h1><?php the_title(); ?></h1>
-
-          <?php the_content(); ?>
-
+        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+          <div class="grid-row padding-bottom-basic color-blue">
+            <div class="grid-item item-s-12">
+              <h1 class="font-size-mid"><?php the_title(); ?></h1>
+            </div>
+            <div class="grid-item item-s-12">
+              <?php echo !empty($year) ? '(' . $year . ')' : '&nbsp;'; ?> • <?php echo !empty($duration) ? $duration . ' minutos' : '&nbsp;'; ?>
+            </div>
+            <div class="grid-item item-s-12">
+              <?php echo !empty($directors) ? $directors[0]->name : '&nbsp;'; ?>
+            </div>
+          </div>
+          <div class="grid-row padding-bottom-basic">
+            <div class="grid-item">
+              <?php the_post_thumbnail(); ?>
+            </div>
+          </div>
+          <div class="grid-row">
+            <div class="grid-item">
+              <?php the_content(); ?>
+            </div>
+          </div>
         </article>
 
 <?php
