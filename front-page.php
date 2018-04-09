82cf80172fce8744a088f2d973e44b7e7f774c10
mikeyrayvon <mikey@interglobal.vision>
2018-04-09 12:03:31 -0500

    update frontpage styling and content

--- front-page.php
+++ front-page.php
@@ -2,6 +2,8 @@
 get_header();
 ?>
 
+<div id="light"><?php //get_template_part('partials/light'); ?><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/ceiling.png"></div>
+
 <main id="main-content">
   <section id="posts">
     <div class="container">
@@ -17,11 +19,11 @@ if (have_posts()) {
     $long_description = get_post_meta($post_id, '_igv_home_description', true);
 ?>
 
-        <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
+        <div <?php post_class('grid-item item-s-12 item-m-9 offset-m-1 item-l-6 padding-bottom-mid p-max-width'); ?> id="post-<?php the_ID(); ?>">
 
           <?php the_content(); ?>
 
-        </article>
+        </div>
 
 <?php
   }
@@ -40,36 +42,58 @@ $args = array(
 $program_query = new WP_Query( $args );
 
 if ( $program_query->have_posts() ) {
+?>
+      <section class="padding-bottom-mid">
+<?php
 	while ( $program_query->have_posts() ) {
 		$program_query->the_post();
 
     $film_array = get_post_meta($post->ID, '_igv_program_group', true);
 
     if (!empty($film_array)) {
-      $date = get_post_meta($post->ID, '_igv_program_date', true);
-      $time = get_post_meta($post->ID, '_igv_program_time', true);
+      $datetime = get_post_meta($post->ID, '_igv_program_date', true);
       $venue = get_post_meta($post->ID, '_igv_program_venue', true);
       $qa = get_post_meta($post->ID, '_igv_program_qa', true);
+      $order = get_post_meta($post->ID, '_igv_order_number', true);
 ?>
-      <div id="program-<?php the_title; ?>">
-        <div class="grid-row">
-          <div class="grid-item">
-            <?php the_title(); ?>
+        <div id="program-<?php the_title; ?>" class="margin-bottom-basic">
+          <div class="grid-row color-blue margin-bottom-small">
+            <div class="grid-item item-s-10 offset-s-2 grid-row margin-bottom-tiny no-gutter">
+              <div class="grid-item item-s-12 grid-row margin-bottom-tiny no-gutter">
+                <div class="grid-item item-s-4 font-uppercase">
+                  <?php echo !empty($datetime) ? date_i18n('l', $datetime) : '&nbsp;'; ?>
+                </div>
+                <div class="grid-item item-s-4 font-uppercase">
+                  <?php echo !empty($datetime) ? date_i18n('j F', $datetime) : '&nbsp;'; ?>
+                </div>
+                <div class="grid-item item-s-4 font-uppercase">
+                  <?php echo !empty($datetime) ? date_i18n('g:i A', $datetime) : '&nbsp;'; ?>
+                </div>
+              </div>
+              <div class="grid-item item-s-12 grid-row no-gutter">
+                <div class="grid-item item-s-4">
+                  <?php echo !empty($venue) ? $venue : '&nbsp;'; ?>
+                </div>
+                <div class="grid-item item-s-8">
+                  <?php echo ($qa === 'qa') ? 'Q & A' : ''; ?>
+                  <?php echo ($qa === 'qa_with') ? 'Q & A con traducción' : ''; ?>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="grid-item">
-            <?php echo !empty($date) ? $date : '&nbsp;'; ?>
+          <div class="grid-row margin-bottom-small">
+            <div class="grid-item item-s-2 color-blue align-self-center">
+              Programa<?php echo !empty($order) ? ' ' . $order : ''; ?>
+            </div>
+            <div class="grid-item item-s-10">
+              <h2 class="font-size-large"><?php the_title(); ?></h2>
+            </div>
           </div>
-          <div class="grid-item">
-            <?php echo !empty($time) ? $time : '&nbsp;'; ?>
-          </div>
-          <div class="grid-item">
-            <?php echo !empty($venue) ? $venue : '&nbsp;'; ?>
-          </div>
-          <div class="grid-item">
-            <?php echo ($qa === 'qa') ? 'Q & A' : ''; ?>
-            <?php echo ($qa === 'qa_with') ? 'Q & A con traducción' : ''; ?>
-          </div>
-        </div>
+          <div class="grid-row margin-bottom-small">
+            <div class="grid-item item-s-2 color-blue">
+              Titulos
+            </div>
+            <div class="grid-item item-s-10 grid-row no-gutter">
 <?php
       foreach($film_array as $film_post) {
         $film_id = $film_post['film_id'];
@@ -78,25 +102,31 @@ if ( $program_query->have_posts() ) {
         $permalink = get_the_permalink($film_id);
         $duration = get_post_meta($film_id, '_igv_film_duration', true);
         $year = get_post_meta($film_id, '_igv_film_year', true);
+        $directors = wp_get_post_terms($film_id, 'director');
 ?>
-        <div class="grid-row">
-          <div class="grid-item item-s-4">
-            <a href="#" class="js-open-film" data-url="<?php echo $permalink; ?>"><?php echo $title; ?></a>
-          </div>
-          <div class="grid-item item-s-4">
-            <?php echo !empty($year) ? $year : '&nbsp;'; ?>
-          </div>
-          <div class="grid-item item-s-4">
-            <?php echo !empty($duration) ? $duration . ' mins.' : '&nbsp;'; ?>
-          </div>
-        </div>
+              <div class="grid-item item-s-6 grid-row">
+                <div class="grid-item item-s-12 no-gutter">
+                  <a class="u-pointer js-open-film link-underline font-size-mid" data-url="<?php echo $permalink; ?>"><?php echo $title; ?></a>
+                </div>
+                <div class="grid-item item-s-12 no-gutter color-blue">
+                  <?php echo !empty($year) ? '(' . $year . ')' : '&nbsp;'; ?> • <?php echo !empty($duration) ? $duration . ' minutos' : '&nbsp;'; ?>
+                </div>
+                <div class="grid-item item-s-12 no-gutter color-blue">
+                  <?php echo !empty($directors) ? $directors[0]->name : '&nbsp;'; ?>
+                </div>
+              </div>
 <?php
       }
 ?>
-      </div>
+            </div>
+          </div>
+        </div>
 <?php
     }
 	}
+?>
+      </section>
+<?php
 }
 
 wp_reset_postdata();
@@ -105,7 +135,7 @@ wp_reset_postdata();
 if (!empty($long_description)) {
 ?>
       <div class="grid-row">
-        <div class="grid-item">
+        <div class="grid-item item-s-12 item-m-9 item-l-6 p-max-width">
           <?php echo apply_filters('the_content', $long_description); ?>
         </div>
       </div>
@@ -114,8 +144,14 @@ if (!empty($long_description)) {
 ?>
     </div>
   </section>
-  <div id="drawer">
 
+  <div id="close-overlay" class="u-pointer"></div>
+  <div id="drawer" class="padding-top-large padding-bottom-large">
+    <div class="drawer-container">
+      <div id="close-drawer" class="u-pointer">X</div>
+      <div id="drawer-content">
+      </div>
+    </div>
   </div>
 </main>
 
